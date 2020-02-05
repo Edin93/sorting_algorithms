b000720610060c0b79d072bf07c7ee37f636195b
edin93 <il_matador@hotmail.fr>
2020-02-05 16:41:06 +0100

    task 3 (quick sort) done

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,20 +1,85 @@
 #include "sort.h"
+void sort(int *array, int size, int s, int e);
+void swap(int *h, int *k);
+int arrange(int *array, int size, int s, int e);
+
 /**
-* quick_sort - function that sorts an array of integers
-* in ascending order using the Quick sort algorithm.
-* @array: array of integers.
-* @size: the size of the array.
-*/
+ * swap - swap h with k.
+ * @h: first element to swap.
+ * @k: second element to swap.
+ * Return: nothing.
+ */
+void swap(int *h, int *k)
+{
+	int tmp;
+
+	tmp = *h;
+	*h = *k;
+	*k = tmp;
+}
+
+/**
+ * sort - recursive quick sort function.
+ * @array: array of int to sort in increasing order.
+ * @size: array size.
+ * @s: start index.
+ * @e: end index.
+ * Return: nothing.
+ */
+void sort(int *array, int size, int s, int e)
+{
+	int p;
+
+	if (s < e)
+	{
+		p = arrange(array, size, s, e);
+		sort(array, size, s, p - 1);
+		sort(array, size,  p + 1, size - 1);
+	}
+}
+
+/**
+ * arrange - recursive quick sort function.
+ * @array: array of int to sort in increasing order.
+ * @size: array size.
+ * @s: start index.
+ * @e: end index.
+ * Return: nothing.
+ */
+int arrange(int *array, int size, int s, int e)
+{
+	int h, k, p;
+
+	p = e;
+	h = s;
+	for (k = s; k < e; k++)
+	{
+		if (array[k] < array[p])
+		{
+			if (k != h)
+			{
+				swap(&array[h], &array[k]);
+				print_array(array, size);
+			}
+			h++;
+		}
+	}
+	if (h != k)
+	{
+		swap(&array[p], &array[h]);
+		print_array(array, size);
+	}
+	return (h);
+}
+
+/**
+ * quick_sort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm.
+ * @array: array of integers.
+ * @size: the size of the array.
+ * Return: nothing.
+ */
 void quick_sort(int *array, size_t size)
 {
-int i, j, tmp;
-int pivot = array[i];
-for (i = 0; i < (size - 1); i++)
-{
-for (j = i; j < size; j++)
-{
-if (array[j] < pivot)
-{
-}
-}
+	sort(array, size, 0, size - 1);
 }
