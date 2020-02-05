4d98f0e41b40e73c41ac825c296b3161de7efb64
edin93 <il_matador@hotmail.fr>
2020-02-05 20:04:28 +0100

    shell sort first commit

---
+++ 100-shell_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * shell_sort - sort an array in ascending order using the shell sort algo.
+ * @array: the array to sort.
+ * @size: array size.
+ * Return: nothing.
+ */
+void shell_sort(int *array, size_t size)
+{
+}
