8a16e2695330431e1ac4d5ed04b1d0f887a3f71f
rakiasomai <1069@holbertonschool.com>
2020-02-05 21:15:45 +0000

    commit

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,6 @@
+#include "sort.h"
+/**
+* cocktail_sort_list - a function that sorts a doubly linked list
+* of integers in ascending order using the Cocktail shaker sort algorithm.
+* @list: list to print.
+void cocktail_sort_list(listint_t **list)
